8bb7d54b8319b7730f5e503af82893a6412b838b
Aaron Hutton <aaronh8472@gmail.com>
2012-12-21 14:40:09 +0000

    Magic Quotes removal

--- eng/boot.php
+++ eng/boot.php
@@ -1,25 +1,44 @@
 <?php
+
 define("BOOT", "yes");
 
 function return_boot()
 {
     return BOOT;
 }
+
+//Magic Quotes
+# If Magic Quotes are enabled, strip our arrays
+if (get_magic_quotes_gpc())
+{
+    foreach ($_GET as $key => $value)
+        $_GET[$key] = stripslashes($value);
+
+    foreach ($_REQUEST as $key => $value)
+        $_REQUEST[$key] = stripslashes($value);
+
+    foreach ($_POST as $key => $value)
+        $_POST[$key] = stripslashes($value);
+
+    foreach ($_COOKIE as $key => $value)
+        $_COOKIE[$key] = stripslashes($value);
+}
+
 require("registry.php");
 
 //Database Check
 $mysql = new mysqli(REGISTRY_DBVALUES_SERVER, REGISTRY_DBVALUES_USERNAME, REGISTRY_DBVALUES_PASSWORD, REGISTRY_DBVALUES_DATABASE);
 
-    if ($mysql->connect_errno > 0)
-    {
-        echo "SQL Error -- couldn't connect!";
-        exit();
-    }
-    
+if ($mysql->connect_errno > 0)
+{
+    echo "SQL Error -- couldn't connect!";
+    exit();
+}
+
 $mysql->close();
 //End
 
-require(REGISTRY_ENGINE_PATH."classes/classes.php");
+require(REGISTRY_ENGINE_PATH . "classes/classes.php");
 
 require("eng/functions.php");
 
