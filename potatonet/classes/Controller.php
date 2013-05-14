9e42ceed6d4bb28e86ed4c15b10cc81b023b8cc3
Aaron Hutton <aaronh8472@gmail.com>
2013-05-14 21:07:03 +0100

    Added abstract function for adding validation errors

--- potatonet/classes/Controller.php
+++ potatonet/classes/Controller.php
@@ -3,41 +3,46 @@
 abstract class Controller
 {
 
-    protected $model, $view, $inner_view=null, $page, $page_title=null;
-    
-    protected $validation_errors = array();
+	protected $model, $view, $inner_view = null, $page, $page_title = null;
+	protected $validation_errors = array();
 
-    public function getModel()
-    {
-        return $this->model;
-    }
+	public function getModel()
+	{
+		return $this->model;
+	}
 
-    public function getView()
-    {
-        return $this->view;
-    }
-    
-    public function getPageTitle()
-    {
-        return $this->page_title;
-    }
-    
-    public function getInnerView()
-    {
-        return $this->inner_view;
-    }
+	public function getView()
+	{
+		return $this->view;
+	}
 
-    public function getPage()
-    {
-        return $this->page;
-    }
-    
-    public function getValidationErrors()
-    {
-        return $this->validation_errors;
-    }
+	public function getPageTitle()
+	{
+		return $this->page_title;
+	}
 
-    abstract public function execute();
+	public function getInnerView()
+	{
+		return $this->inner_view;
+	}
+
+	public function getPage()
+	{
+		return $this->page;
+	}
+
+	public function getValidationErrors()
+	{
+		return $this->validation_errors;
+	}
+
+	abstract public function execute();
+
+	public function addValidationError($validation_error)
+	{
+		$this->validation_errors[] = $validation_error;	
+	}
+
 }
 
 ?>
