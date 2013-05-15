26fbb2350d90528c1b5589bda56719be02c781a3
Aaron Hutton <aaronh8472@gmail.com>
2013-05-15 21:23:48 +0100

    Added validator class

---
+++ potatonet/classes/Validator.php
@@ -0,0 +1,82 @@
+<?php
+class Validator
+{
+	private $string_to_test = NULL;
+	private $is_valid = true;
+	private $max_string_length = -1;
+	private $min_string_length = -1;
+	private $cannot_be_empty = false;
+	private $error_string = array();
+
+	public function __construct($string_to_test)
+	{
+		$this->string_to_test = $string_to_test;
+	}
+
+	public function validateString()
+	{
+		if($this->max_string_length != -1)
+			$this->checkLenthMaxValid();
+
+		if($this->min_string_length != -1)
+			$this->checkLenthMinValid();
+
+		if($this->cannot_be_empty)
+			$this->checkStringEmpty();
+	}
+
+	public function getIsValid()
+	{
+		return $this->is_valid;
+	}
+
+	public function getErrorArray()
+	{
+		return $this->error_string;
+	}
+
+	public function setMaxStringLength($max_string_length)
+	{
+		$this->max_string_length = $max_string_length;
+	}
+
+	public function setMinStringLength($min_string_length)
+	{
+		$this->min_string_length = $min_string_length;
+	}
+
+	public function setCannotBeEmpty($cannot_be_empty)
+	{
+		$this->cannot_be_empty = $cannot_be_empty;
+	}
+
+	private function checkLenthMaxValid()
+	{
+		if(strlen($this->string_to_test) > $this->max_string_length)
+		{
+			$this->is_valid = false;
+			$this->error_string[] = "too long (over ".$this->max_string_length." characters)";
+		}
+	}
+	
+	private function checkLenthMinValid()
+	{
+		if(strlen($this->string_to_test) < $this->min_string_length)
+		{
+			$this->is_valid = false;
+			$this->error_string[] = "too short (under ".$this->min_string_length." characters)";
+		}
+	}
+
+	private function checkStringEmpty()
+	{
+		if(trim($this->string_to_test) == null)
+		{
+			$this->is_valid = false;
+			$this->error_string[] = "string is empty";
+		}
+	}
+
+	
+}
+?>
--- potatonet/classes/classes.php
+++ potatonet/classes/classes.php
@@ -4,6 +4,7 @@ require_once(REGISTRY_ENGINE_PATH."classes/Entity.php");
 require_once(REGISTRY_ENGINE_PATH."classes/User.php");
 require_once(REGISTRY_ENGINE_PATH."classes/LoggedInUser.php");
 require_once(REGISTRY_ENGINE_PATH."classes/Controller.php");
+require_once(REGISTRY_ENGINE_PATH."classes/Validator.php");
 
 if(file_exists(REGISTRY_ENGINE_PATH."classes/script.php"))
     include (REGISTRY_ENGINE_PATH."classes/script.php");
