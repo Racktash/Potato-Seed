50bf9837b6d199bc8e4c1e803092315719f0b0f6
Aaron Hutton <aaronh8472@gmail.com>
2013-07-17 19:38:43 +0100

    Replaced /eng directory with loader class

---
+++ src/PotatoSeed/Seed_Loader.php
@@ -0,0 +1,60 @@
+<?php
+
+class Seed_Loader
+{
+
+	public static function initSeed()
+	{
+		if (get_magic_quotes_gpc())
+			self::stripMagicQuotes();
+
+		self::loadDataContainers();
+		self::checkDatabase();
+		self::loadHelperFunctions();
+		self::loadVersionFile();
+		self::loadLoggedIn();
+	}
+
+	private static function stripMagicQuotes()
+	{
+		foreach ($_GET as $key => $value)
+			$_GET[$key] = stripslashes($value);
+
+		foreach ($_REQUEST as $key => $value)
+			$_REQUEST[$key] = stripslashes($value);
+
+		foreach ($_POST as $key => $value)
+			$_POST[$key] = stripslashes($value);
+
+		foreach ($_COOKIE as $key => $value)
+			$_COOKIE[$key] = stripslashes($value);
+	}
+
+	private static function loadDataContainers()
+	{
+		require(REGISTRY_ENGINE_PATH . "datacontainers/User.php");
+	}
+
+	private static function checkDatabase()
+	{
+		echo "TODO -- check database!";
+	}
+
+	private static function loadHelperFunctions()
+	{
+		require(REGISTRY_ENGINE_PATH . "functions.php");
+	}
+
+	private static function loadVersionFile()
+	{
+		require("version.php");
+	}
+
+	private static function loadLoggedIn()
+	{
+		require(REGISTRY_ENGINE_PATH . "loggedin.php");
+	}
+
+}
+
+?>
--- src/login.php
+++ src/login.php
@@ -1,5 +1,10 @@
 <?php
-require("eng/boot.php");
+require("registry.php");
+require(REGISTRY_ENGINE_PATH."Seed_Loader.php");
+Seed_Loader::initSeed();
+
+
+
 require_once(REGISTRY_SEEDBASE_PATH."controllers/login.php");
 
 if (REGISTRY_CAN_LOGIN)
