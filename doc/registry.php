7192277e8e8cfc26ba3418eef1af74be5c3f4816
Aaron Hutton <aaronh8472@gmail.com>
2013-08-09 21:58:46 +0100

    Added registration functional regression suite Moved all which can be tested using automated scriot out of manual test cases.
    Reserving testing valid users for manual.
    
    Included documentation source files.

---
+++ doc/registry.php
@@ -0,0 +1,14 @@
+<?php
+
+//Paths
+define("REGISTRY_CODE_PATH", "PotatoDocs/");
+define("REGISTRY_DATA_PATH", "data/");
+
+//Site Details
+define("REGISTRY_SITE_TITLE", "Potato Seed Documentation");
+
+//Site Display Settings
+define("REGISTRY_SITE_TEMPLATE", "docs2013.html");
+define("REGISTRY_SITE_STYLESHEET", "docs2013.css");
+
+?>
